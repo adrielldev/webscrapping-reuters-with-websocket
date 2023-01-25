b497fdc6e4ba3f7cb9f4065e1e86337a13b4e6e6
adriel <adrieldev@outlook.com>
2023-01-25 19:32:24 -0300

    feat-deploy

--- server/scrap.js
+++ server/scrap.js
@@ -1,3 +1,5 @@
+const { link } = require('fs');
+const { title } = require('process');
 const puppeteer = require('puppeteer')
 const db = require('./db');
 
@@ -8,21 +10,31 @@ const scrapData = async () => {
   await page.goto('https://www.reuters.com/markets/currencies/', { waitUntil: 'domcontentloaded' });
   
   const scrappedData = await page.evaluate(()=>{
+    let title; let link; let time;
+    const divLi = document.querySelector('.spacing-container__container__2g5QT > ul > li > div')
+    if(divLi.classList.contains('media-story-card__hero__2nvVj')){
+      title = document.querySelector('.media-story-card__body__3tRWy > a > span').innerText
+      link = document.querySelector('.media-story-card__body__3tRWy > a').href
+      time = document.querySelector('.media-story-card__body__3tRWy >  time').dateTime
+    }else{
+      title = document.querySelector('.text-story-card__playlist__3Ls10 > a ').innerText
+      link = document.querySelector('.text-story-card__playlist__3Ls10 > a').href
+      time = document.querySelector('.text-story-card__playlist__3Ls10 > time').dateTime
+    }
     
-    const title = document.querySelector('.media-story-card__body__3tRWy > a > span').innerText
-    const link = document.querySelector('.media-story-card__body__3tRWy > a').href
-    const time = document.querySelector('.media-story-card__body__3tRWy >  time').dateTime
     const obj = {
       title:title,
       link:link,
       time:time
     }
+    
     return obj
   })
+ 
+  await db.query(`TRUNCATE scrap`);
   let sql = `
    INSERT INTO scrap(title,link,time)
    VALUES($1,$2,$3)
-   ON CONFLICT (link) DO NOTHING
    `;
   let params = [scrappedData.title, scrappedData.link, scrappedData.time]
 
@@ -31,5 +43,6 @@ const scrapData = async () => {
   await browser.close();
 
 };
+
 
 module.exports = scrapData
